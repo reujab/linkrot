5caa9afdd3b4f4ee9140cbdc2dedeba35d537a7a
reujab <reujab@gmail.com>
2017-07-27 19:58:10 -0400

    read files recursively

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	die(filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		// skip version control
+		if info.Name() == ".git" {
+			return filepath.SkipDir
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		file, err := os.Open(path)
+		die(err)
+		defer func() { die(file.Close()) }()
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			// TODO: read line
+		}
+		return scanner.Err()
+	}))
+}
+
+func die(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
