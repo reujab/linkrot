6f9b1f9185a4663251ca90ef07d11e41c858e38e
reujab <reujab@gmail.com>
2017-07-27 22:42:27 -0400

    added flags

--- main.go
+++ main.go
@@ -10,6 +10,13 @@ import (
 )
 
 var (
+	verbose    bool
+	noHTTPS    bool
+	noStatus   bool
+	noRedirect bool
+)
+
+var (
 	waitgroup = new(sync.WaitGroup)
 	semaphore = make(chan interface{}, 5)
 )
@@ -25,12 +32,40 @@ var client = &http.Client{
 func main() {
 	app := cli.NewApp()
 	app.Usage = "a command-line app that finds broken hyperlinks in files"
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:        "verbose",
+			Usage:       "print successful requests",
+			Destination: &verbose,
+		},
+		cli.BoolFlag{
+			Name:        "no-https",
+			Usage:       "don't check if domain supports https",
+			Destination: &noHTTPS,
+		},
+		cli.BoolFlag{
+			Name:        "no-status",
+			Usage:       "don't check http status",
+			Destination: &noStatus,
+		},
+		cli.BoolFlag{
+			Name:        "no-redirect",
+			Usage:       "don't check for redirects",
+			Destination: &noRedirect,
+		},
+	}
 	app.Commands = []cli.Command{
 		{
 			Name:      "walk",
 			Usage:     "Walks a directory and checks every file",
 			UsageText: app.Name + " walk [directory]",
-			Action:    cmdWalk,
+			Flags: []cli.Flag{
+				cli.StringSliceFlag{
+					Name:  "exclude",
+					Usage: "regular expressions to exclude",
+				},
+			},
+			Action: cmdWalk,
 		},
 	}
 	app.Run(os.Args)
